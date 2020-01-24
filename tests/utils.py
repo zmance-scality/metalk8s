c41c3a38dec2661731ff98d06546d175d4245535
Alexandre Allard <alexandre.allard@scality.com>
2020-01-24 08:53:09 +0100

    tests: add function to get node control plane ip
    Add a function to help retrieve control plane IP
    of a node through SSH using salt-call command.
    This function is needed in order to retrieve
    the IP of an API Server for the restoration of
    the bootstrap

--- tests/utils.py
+++ tests/utils.py
@@ -52,12 +52,21 @@ def get_ip_from_cidr(host, cidr):
     return None
 
 
+def get_node_grain(grain, nodename, ssh_config):
+    node = testinfra.get_host(nodename, ssh_config=ssh_config)
+    with node.sudo():
+        return node.check_output(
+            'salt-call --local --out txt grains.get {} | cut -c 8-'
+            .format(grain)
+        )
+
+
 def get_node_name(nodename, ssh_config=None):
     """Get a node name (from SSH config)."""
     if ssh_config is not None:
-        node = testinfra.get_host(nodename, ssh_config=ssh_config)
-        with node.sudo():
-            return node.check_output(
-                'salt-call --local --out txt grains.get id | cut -c 8-'
-            )
+        return get_node_grain('id', nodename, ssh_config)
     return nodename
+
+
+def get_node_control_plane_ip(nodename, ssh_config):
+    return get_node_grain('metalk8s:control_plane_ip', nodename, ssh_config)
