d1b90bee974a10a6de54f414594c65415150427e
Guillaume Demonet <guillaume.demonet@scality.com>
2020-07-31 19:10:04 +0200

    salt/tests: Add unit tests for `metalk8s_drain`
    [WIP] to be split
    
    Introduces:
    - an API mock which allows the tester to manage responses and objects
    directly, while the tested code can interact with it through
    `metalk8s_kubernetes` methods
    - a mock for the `time` module, which allows the drain process to run
    faster than real time, and also controls the API mock through timed
    events
    - a mock for the eviction API
    
    See: #2266

--- salt/tests/unit/utils.py
+++ salt/tests/unit/utils.py
@@ -1,6 +1,10 @@
 """
 Utils, helpers for testing
 """
+import functools
+import operator
+
+from parameterized import param, parameterized
 
 
 def cmd_output(retcode=0, stdout=None, stderr=None, pid=12345):
@@ -13,3 +17,39 @@ def cmd_output(retcode=0, stdout=None, stderr=None, pid=12345):
         'stdout': stdout or '',
         'stderr': stderr or ''
     }
+
+
+def parameterized_from_cases(test_cases):
+    return parameterized.expand(
+        param.explicit(kwargs=test_case) for test_case in test_cases
+    )
+
+
+def split_path(path, delimiter="."):
+    return (
+        int(key) if key.isdigit() else key for key in path.split(delimiter)
+    )
+
+
+def get_dict_element(data, path, delimiter="."):
+    """Traverse a nested dict with a compound path."""
+    return functools.reduce(
+        operator.getitem, split_path(path, delimiter), data
+    )
+
+
+def set_dict_element(data, path, value, delimiter="."):
+    """Set a value in a nested dict with a compound path."""
+    current = data
+
+    # TODO(py3): use syntax "*prev_keys, last_key = split_path(...)"
+    keys = list(split_path(path, delimiter))
+    for key in keys[:-1]:
+        try:
+            current = current[key]
+        except IndexError:
+            current = []
+        except KeyError:
+            current = current.setdefault(key, {})
+
+    current[keys[-1]] = value
